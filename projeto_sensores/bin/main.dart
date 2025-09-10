e6df059c876004b883dcd4303a5030c4ae117254
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-09-09 21:52:47 -0300

    Organização de todos os arquivos no conceito de POO, onde todos possuem seus próprios arquivos, e o main gerencia tudo/dá o start da execução do código.

--- projeto_sensores/bin/main.dart
+++ projeto_sensores/bin/main.dart
@@ -1,36 +1,5 @@
-import 'dart:async';
-import 'dart:io';
-import 'dashboard.dart';
-import 'utils.dart';
+import 'package:projeto_sensores/dashboard.dart';
 
-void main(List<String> args) {
-  print('Iniciando aplicação...');
-
-  // Inicia timer simulado para gerar leituras a cada 2 segundos
-  Timer.periodic(Duration(seconds: 2), (timer) {
-    var leitura = gerarLeituraSimulada();
-    leituras.add(leitura);
-  });
-
-  // Loop principal de menu
-  while (true) {
-    printMenu();
-    var escolha = stdin.readLineSync();
-    if (escolha == null) break;
-
-    if (escolha == '0') {
-      print('Encerrando aplicação...');
-      exit(0);
-    }
-
-    if (['1', '2', '3', '4'].contains(escolha)) {
-      mostrarEstatisticas(escolha);
-      stdout.writeln('\nPressione Enter para voltar ao menu...');
-      stdin.readLineSync();
-      continue;
-    }
-
-    stdout.writeln('Opção inválida. Pressione Enter para tentar novamente.');
-    stdin.readLineSync();
-  }
+void main() {
+  Dashboard().start();
 }
